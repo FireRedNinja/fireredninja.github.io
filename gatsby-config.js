8bfa219e43fdbbe4d2386bbaf5dfb64cf3c06ec0
Noel Rajan <noel.rajan@skyscanner.net>
2021-09-28 08:25:49 +0100

    update packages and update theme colour

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   siteMetadata: {
     title: `Noel Rajan's Portfolio`,
-    description: `Site to showcase my projects.`,
     author: `Noel Rajan`,
     siteUrl: `https://fireredninja.github.io/`
   },
@@ -23,11 +22,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Noel Rajan`,
+        short_name: `Noel`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#374151`,
+        theme_color: `#374151`,
         display: `minimal-ui`,
         icon: `src/images/code-icon.png`, // This path is relative to the root of the site.
       },
