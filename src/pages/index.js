8a76892defa8b4fd8aab1f1f169508b4d6c26f88
Noel Rajan <leonr13@hotmail.co.uk>
2022-09-06 14:34:08 +0100

    update packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,9 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 import Projects from '../components/projects';
-import ReactComment from '../components/reactComment';
 
 const IndexPage = () => (
   <Layout>
-    <ReactComment
-      text={`Favicon - coding by shuai tawf from the Noun Project`}
-    />
     <SEO title="Home" />
     <Projects />
   </Layout>
